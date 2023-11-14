97136c4ba38fa9dffe47cab75e2fa6a6c2fdc060
Stringycheese <150865248+Stringycheese@users.noreply.github.com>
2023-11-14 23:20:07 +0000

    Create name.py

---
+++ name.py
@@ -0,0 +1,9 @@
+name = "Caitlin"
+print("Hello, my name is " + name + ".")
+surname = "Brooks"
+print("Hello, my name is " + name + " " + surname + ".")
+full_name = name + " " + surname
+print("Hello, my name is " + full_name)
+print("Hello, my name is " + full_name.lower())
+print("Hello, my name is " + full_name.upper())
+print("Hello, my name is " + (name + " ")*3 + (surname + " ")*3)
